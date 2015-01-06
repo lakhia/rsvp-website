0c23dff80548c075c90c3fea2f2f6aa53dbfeebf
_tahabi <tahabi@live.com>
2015-01-06 17:49:32 +0530

    skeleton files for remaining features

---
+++ admin.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('aux.php');
+
+function insert_week($db, $date, $details)
+{
+	//$edate = $db->real_escape_string($date);
+	//$edetails = $db->real_escape_string($details);
+
+	$sql = "INSERT INTO `week` (`date`, `details`) VALUES ('" . $date ."', '" . $details . "');";
+
+	$msg = ( !$db->query($sql) ) ? $db->error : "Successfully added meal on " . $date .".";
+
+	echo Helper::convert_array_to_json("", $msg);
+
+	return true;
+}
+
+function delete_week($db, $date)
+{
+	$sql = "DELETE FROM `week` WHERE `date` = '" . $date . "';";
+
+	$msg = ( !$db->query($sql) ) ? $db->error : "Successfully deleted meal on " . $date . ".";
+
+	echo Helper::convert_array_to_json("", $msg);
+}
+
+
+?>
