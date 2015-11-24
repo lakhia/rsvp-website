6753796d0b3be27e41a604770b6f2d216e33e014
Ali Lakhia <lakhia@gmail.com>
2015-11-24 10:34:18 -0800

    Do not show RSVP for days that are not enabled or have no details

--- app/print.php
+++ app/print.php
@@ -17,21 +17,24 @@ function print_filling($db) {
     }
     $from = Helper::get_day($offset);
 
-    // Make query
-    $query = "SELECT `thaali`, `lastName`, `firstName` FROM `rsvps` " .
-        "LEFT JOIN `family` on family.thaali = rsvps.thaali_id " .
-        "WHERE `rsvp` = 1 AND `date` = '" . $from . "' ORDER BY thaali;";
+    // Get details for date
+    $details = get_details($db, $from);
 
-    $result = $db->query($query);
+    if ($details) {
+        // Get RSVP and family
+        $query = "SELECT `thaali`, `lastName`, `firstName` FROM `rsvps` " .
+            "LEFT JOIN `family` on family.thaali = rsvps.thaali_id " .
+            "WHERE `rsvp` = 1 AND `date` = '" . $from . "' ORDER BY thaali;";
+        $result = $db->query($query);
 
-    // Append first name and last
-    while($row = $result->fetch_assoc()) {
-        $row['name'] = $row['firstName'] . " " . $row['lastName'];
-        unset($row['firstName']);
-        unset($row['lastName']);
-        $rows[] = $row;
+        // Append first name and last
+        while($row = $result->fetch_assoc()) {
+            $row['name'] = $row['firstName'] . " " . $row['lastName'];
+            unset($row['firstName']);
+            unset($row['lastName']);
+            $rows[] = $row;
+        }
     }
-
     // Create message
     if (isset($rows)) {
         $count = count($rows);
@@ -46,4 +49,16 @@ function print_filling($db) {
     Helper::print_to_json($rows, $msg, $from);
 }
 
+function get_details($db, $date) {
+    $query = 'SELECT details,enabled from events where date="' . $date . '";';
+    $result = $db->query($query);
+    if (!$result || $result->num_rows != 1) {
+        return "";
+    }
+    $row = $result->fetch_assoc();
+    if (!$row['enabled']) {
+        return "";
+    }
+    return $row['details'];
+}
 ?>
