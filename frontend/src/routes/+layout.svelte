8eba710f8e594597868868a1ff7025896d616b19
Ali Lakhia <lakhia@gmail.com>
2026-04-24 17:01:51 -0700

    Update events icon

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -17,7 +17,7 @@
     { href: '/plan', label: 'Plan', icon: 'calendar', adminOnly: false },
     { href: '/shop', label: 'Shopping', icon: 'cart', adminOnly: false },
     { href: '/family', label: 'Family', icon: 'users', adminOnly: true },
-    { href: '/event', label: 'Events', icon: 'utensils', adminOnly: true },
+    { href: '/event', label: 'Events', icon: 'fork-knife', adminOnly: true },
     { href: '/measure', label: 'Measure', icon: 'scale', adminOnly: true },
     { href: '/ingred', label: 'Ingredients', icon: 'list', adminOnly: true },
     { onclick: handleLogout, label: 'Logout', icon: 'logout', adminOnly: false },
